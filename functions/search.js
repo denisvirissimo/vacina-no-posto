e17613bdd36b669466488be634cd9ad668d8a874
denisvirissimo <denisoftwares@gmail.com>
2021-07-14 00:44:16 -0300

    Build inicial para primeiro teste da função no netlify,

--- functions/search.js
+++ functions/search.js
@@ -8,11 +8,11 @@ exports.handler = function(event, context, callback) {
         body: JSON.stringify({ message: 'Hello teste' })
     });*/
 
-    const API_URL = 'https://imunizacao-es.saude.gov.br/_search';
-    const API_USER = 'imunizacao_public';
-    const API_PASSWORD = 'qlto5t&7r_@+#Tlstigi';
+    // const API_URL = 'https://imunizacao-es.saude.gov.br/_search';
+    // const API_USER = 'imunizacao_public';
+    // const API_PASSWORD = 'qlto5t&7r_@+#Tlstigi';
 
-    // const { API_URL, API_USER, API_PASSWORD} = process.env;
+    const { API_URL, API_USER, API_PASSWORD} = process.env;
 
     // Send
     const send = body => {
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const fetchData = async () =>
-    await (await fetch('http://localhost:9000/search')).json();
-    // await (await fetch('/.netlify/functions/search')).json();
+    // await (await fetch('http://localhost:9000/search')).json();
+    await (await fetch('/.netlify/functions/search')).json();
 
 fetchData().then(data => {
     console.log(data);
