3f62519b350bb36953562714d0cc1202c99bc2a6
denisvirissimo <denisoftwares@gmail.com>
2021-07-16 02:08:58 -0300

    Correção da URL da API.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const fetchData = async () => {
-    
-    const response = await fetch('http://localhost:9000/search', {
+    // var url = 'http://localhost:9000/search';
+    var url = '/.netlify/functions/search';
+    const response = await fetch(url, {
         method: 'POST',
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache',
@@ -13,8 +14,6 @@ const fetchData = async () => {
       });
       return response.json();
 }
-    // await (await fetch('http://localhost:9000/search')).json();
-    // await (await fetch('/.netlify/functions/search')).json();
 
 fetchData().then(data => {
     console.log(data);
