5d8d52a29154a73caf75944795b7addfc096a6a6
denisvirissimo <denisoftwares@gmail.com>
2021-07-18 19:18:47 -0300

    Minificação do js principal.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify');
 
 gulp.task('default', function (done) {
     gulp.src('./node_modules/choices.js/public/assets/styles/choices.min.css')
         .pipe(gulp.dest('./assets/dist/css'));
     gulp.src('./node_modules/choices.js/public/assets/scripts/choices.min.js')
         .pipe(gulp.dest('./assets/dist/js'));
-
+    gulp.src('js/*.js')
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            }
+        }))
+        .pipe(gulp.dest('js'))
     done();
 });
